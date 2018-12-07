4a92fd4e46a21ada552ee81c82cf1f4b489ed933
lindolo25 <lindolo25@outlook.com>
2018-12-07 17:45:54 -0500

    schema change

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+drop database animals_db;
+
+drop database favorite_db;
+
+drop database greatbay_db;
+
+drop database ice_creamdb;
+
+drop database playlist_db;
+
+drop database programing_db;
+
+drop database seinfeld_db;
+
+drop database task_saver_db;
+
+drop database wizard_schools_db;
